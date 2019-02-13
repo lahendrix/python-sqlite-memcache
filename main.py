9191b8e126b0b23e0b3492319f54b2808e4f11c2
Larry A. Hendrix <lahendrix@uwalumni.com>
2019-02-12 22:22:20 -0600

    Add implementation for get and set commands.

--- main.py
+++ main.py
@@ -10,20 +10,23 @@ PORT = 11211
 
 class Memcache():
 
-    create_table_sql = (
+    _create_table_sql = (
         'CREATE TABLE IF NOT EXISTS cache '
         '('
         '  key TEXT PRIMARY KEY,'
         '  val BLOB'
         ')'
     )
-    get_sql = 'SELECT val FROM cache WHERE key = ?'
-    del_sql = 'DELETE FROM cache WHERE key = ?'
-    set_sql = 'REPLACE INTO cache (key, val) VALUES (?, ?)'
-    add_sql = 'INSERT INTO cache (key, val) VALUES (?, ?)'
+    _get_sql = 'SELECT val FROM cache WHERE key = ?'
+    _del_sql = 'DELETE FROM cache WHERE key = ?'
+    _add_sql = 'INSERT INTO cache (key, val) VALUES (?, ?)'
 
     db_connection = None
     socket_connection = None
+
+    def __init__(self, db_file):
+        self.create_connection(db_file)
+        self.create_server()
 
     def create_server(self):
         """ create a server that listens on
@@ -41,7 +44,19 @@ class Memcache():
                     data = self.socket_connection.recv(1024)
                     if not data:
                         break
-                    self.socket_connection.sendall(data)
+                    data = data.decode('utf-8').split()
+                    self.process_command(data)
+
+    def process_command(self, data):
+        if data[0] == 'get':
+            self.get(data)
+        elif data[0] == 'set':
+            self.set(data)
+        elif data[0] == 'delete':
+            self.delete(data)
+        else:
+            self.socket_connection.send('Invalid command.\n')
+            return None
 
     def create_connection(self, db_file):
         """ create a database connection to the SQLite database
@@ -49,24 +64,47 @@ class Memcache():
         :param db_file: database file
         :return: Connection object or None
         """
+        print('creating db connection')
         try:
             self.db_connection = sqlite3.connect(db_file)
+            self.db_connection.execute(self._create_table_sql)
+            print('Ran the create table')
         except Error as e:
             print(e)
 
         return None
 
-    def get(self, key):
+    def get(self, data):
         # Retrieve a property from cache
-        return
+        if len(data) > 1:
+            key = data[1]
+            for row in self.db_connection.execute(self._get_sql, (key,)):
+                self.socket_connection.send(row[0].encode('utf-8'))
+            return
+        self.socket_connection.send('Invalid get command'.encode('utf-8'))
 
     def delete(self, key):
         # Delete property from cache
-        return
+        if len(data) > 1:
+            key = data[1]
+            self.socket_connection.send('issued delete {key}\n'.format(key=key).encode('utf-8'))
+            return
+        self.socket_connection.send('Invalid delete command'.encode('utf-8'))
 
-    def set(self, key, value):
+    def set(self, data):
         # Set a property in cache
-        return
+        if len(data) > 2:
+            key = data[1]
+            value = data[2]
+            with self.db_connection as conn:
+                try:
+                    conn.execute(self._add_sql, (key, value))
+                except sqlite3.IntegrityError:
+                    # call the update method as fallback
+                    print('Attempting to set an existing key {k}. Falling back to update method.')
+                    pass
+                return
+        self.socket_connection.send('Invalid set command'.encode('utf-8'))
 
     def show(self):
         # Show all values from cache
@@ -78,5 +116,5 @@ if __name__ == '__main__':
         cache = Memcache()
         cache.show()
         sys.exit(1)
-    cache = Memcache()
-    cache.create_server()
+    elif len(sys.argv) == 3 and sys.argv[1] == 'serve':
+        cache = Memcache(sys.argv[2])
