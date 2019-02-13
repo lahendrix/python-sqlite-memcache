f74c1f259e8fc88faaec0894dbaac2e9431e9c1d
Larry A. Hendrix <lahendrix@uwalumni.com>
2019-02-12 23:56:44 -0600

    Small cleanup.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import sqlite3
 from sqlite3 import Error
 import socket
+from utils import encode, format_row
 
 HOST = 'localhost'
 PORT = 11211
@@ -23,8 +24,8 @@ class Memcache():
     _add_sql = 'INSERT INTO cache (key, val) VALUES (?, ?)'
     _show_sql = 'SELECT key, val from cache'
 
-    db_connection = None
-    socket_connection = None
+    db_conn = None
+    socket_conn = None
 
     def __init__(self, db_file, start_server):
         self.create_connection(db_file)
@@ -40,11 +41,11 @@ class Memcache():
             s.bind((HOST, PORT))
             print('server started at {host}:{port}'.format(host=HOST, port=PORT))
             s.listen(5)
-            self.socket_connection, addr = s.accept()
-            with self.socket_connection:
+            self.socket_conn, addr = s.accept()
+            with self.socket_conn:
                 print('Connected by', addr)
                 while True:
-                    data = self.socket_connection.recv(1024)
+                    data = self.socket_conn.recv(1024)
                     if not data:
                         break
                     self.process_command(data)
@@ -63,7 +64,7 @@ class Memcache():
         elif data[0] == 'delete':
             self.delete(data)
         else:
-            self.socket_connection.send('Invalid command.\n')
+            self.socket_conn.send(encode('Invalid command: {command}'.format(command=data[0])))
             return None
 
     def create_connection(self, db_file):
@@ -73,8 +74,8 @@ class Memcache():
         :return: Connection object or None
         """
         try:
-            self.db_connection = sqlite3.connect(db_file)
-            self.db_connection.execute(self._create_table_sql)
+            self.db_conn = sqlite3.connect(db_file)
+            self.db_conn.execute(self._create_table_sql)
         except Error as e:
             print(e)
 
@@ -84,42 +85,41 @@ class Memcache():
         # Retrieve a property from cache
         if len(data) > 1:
             key = data[1]
-            for row in self.db_connection.execute(self._get_sql, (key,)):
-                self.socket_connection.send(row[0].encode('utf-8'))
+            for row in self.db_conn.execute(self._get_sql, (key,)):
+                self.socket_conn.send(encode(row[0]))
             return
-        self.socket_connection.send('Invalid get command'.encode('utf-8'))
+        self.socket_conn.send(encode('Proper usage: get <key>'))
 
     def delete(self, data):
         # Delete property from cache
         if len(data) > 1:
             key = data[1]
-            with self.db_connection as conn:
+            with self.db_conn as conn:
                 conn.execute(self._del_sql, (key,))
                 return
-        self.socket_connection.send('Invalid delete command'.encode('utf-8'))
+        self.socket_conn.send(encode('Proper usage: delete <key>'))
 
     def set(self, data):
         # Set a property in cache
         if len(data) > 2:
             key = data[1]
             value = data[2]
-            with self.db_connection as conn:
+            with self.db_conn as conn:
                 try:
                     conn.execute(self._add_sql, (key, value))
                 except sqlite3.IntegrityError:
                     pass
                 return
-        self.socket_connection.send('Invalid set command'.encode('utf-8'))
+        self.socket_conn.send(encode('Proper usage: set <key> <val>'))
 
     def show(self):
         # Show all values from cache
-        formatted_row = '{:<10} | {:<10}'
         separator = '-' * 20
-        print(formatted_row.format("Key", "Value"))
+        print(format_row(("Key", "Value")))
         print(separator)
-        with self.db_connection as conn:
+        with self.db_conn as conn:
             for row in conn.execute(self._show_sql):
-                print(formatted_row.format(row[0],row[1]))
+                print(format_row(row))
         return
 
 if __name__ == '__main__':
